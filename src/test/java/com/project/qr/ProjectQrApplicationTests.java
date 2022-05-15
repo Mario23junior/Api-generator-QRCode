8fe37595866578d9df89fd855ad7cefd5ff2bdd6
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-15 05:41:16 -0300

    Iniciando projeto de geração de QR code

---
+++ src/test/java/com/project/qr/ProjectQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
