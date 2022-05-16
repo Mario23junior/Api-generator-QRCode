afa7d166ca048113a51a1a2489248d12e334a88e
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-16 03:47:31 -0300

    definindo controller de recebimentos de metricas

---
+++ src/main/java/com/project/qr/controller/qrCodeGenerateController.java
@@ -0,0 +1,32 @@
+package com.project.qr.controller;
+
+import java.io.IOException;
+
+import com.google.zxing.WriterException;
+import com.project.qr.service.GenerationQRcode;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/generate/qrcode/")
+public class qrCodeGenerateController {
+
+	private GenerationQRcode service;
+	
+	public qrCodeGenerateController(GenerationQRcode service) {
+		this.service = service;
+	}
+	
+	private final String PATH_CODE_QRIMAGE = "./src/main/resource/QRCode.png";
+
+	@GetMapping(value = "generat/{width}/{height}/{codeText}")
+	public void capturingContentMetricsQrCode(@PathVariable("codeText") String codeText, @PathVariable("width") Integer width,
+			@PathVariable("height") Integer height) throws IOException, WriterException {
+		
+		service.geneateQRCodeImageMetrics(codeText, width, height, PATH_CODE_QRIMAGE);
+
+	}
+}
