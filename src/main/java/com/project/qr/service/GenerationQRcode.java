570ab2b3cb11a34ad9c39093ebf4acf7f73cf99e
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-16 03:18:35 -0300

    Criando metodo de geracao de metricas por arrays

--- src/main/java/com/project/qr/service/GenerationQRcode.java
+++ src/main/java/com/project/qr/service/GenerationQRcode.java
@@ -1,5 +1,6 @@
 package com.project.qr.service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -22,5 +23,15 @@ public class GenerationQRcode {
 		
 		Path path = FileSystems.getDefault().getPath(filePath);
 		MatrixToImageWriter.writeToPath(bitMatrix, format, path);
+	}
+	
+	public byte[] captureQRCodeImage(String text,Integer width,Integer height) throws WriterException, IOException {
+		QRCodeWriter qrCodeWrite = new QRCodeWriter();
+		BitMatrix bitMatrix = qrCodeWrite.encode(text, BarcodeFormat.QR_CODE, width, height);
+		
+		ByteArrayOutputStream pngOutStream = new ByteArrayOutputStream();
+		MatrixToImageWriter.writeToStream(bitMatrix, format, pngOutStream);
+		byte[] pngDate = pngOutStream.toByteArray();
+		return pngDate;
 	}
 }
