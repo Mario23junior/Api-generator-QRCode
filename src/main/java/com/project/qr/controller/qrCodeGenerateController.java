49f94c0abee723b74cf2ef0f466cf7b7c4c502fc
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-16 03:55:43 -0300

    Criando endpoint de retorno REST

--- src/main/java/com/project/qr/controller/qrCodeGenerateController.java
+++ src/main/java/com/project/qr/controller/qrCodeGenerateController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import com.google.zxing.WriterException;
 import com.project.qr.service.GenerationQRcode;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class qrCodeGenerateController {
 
 	private GenerationQRcode service;
-	
+
 	public qrCodeGenerateController(GenerationQRcode service) {
 		this.service = service;
 	}
-	
+
 	private final String PATH_CODE_QRIMAGE = "./src/main/resource/QRCode.png";
 
 	@GetMapping(value = "generat/{width}/{height}/{codeText}")
-	public void capturingContentMetricsQrCode(@PathVariable("codeText") String codeText, @PathVariable("width") Integer width,
-			@PathVariable("height") Integer height) throws IOException, WriterException {
-		
+	public void capturingContentMetricsQrCode(@PathVariable("codeText") String codeText,
+			@PathVariable("width") Integer width, @PathVariable("height") Integer height)
+			throws IOException, WriterException {
 		service.geneateQRCodeImageMetrics(codeText, width, height, PATH_CODE_QRIMAGE);
+	}
+
+	@GetMapping(value = "/generate/{width}/{height}/{codeText}")
+	public ResponseEntity<byte[]> generateQRCode(@PathVariable("codeText") String codeText,
+			@PathVariable("width") Integer width, @PathVariable("height") Integer height) throws WriterException, IOException {
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(service.captureQRCodeImage(codeText, width, height));
 
 	}
 }
